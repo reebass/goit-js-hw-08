7e89f0ddd0bd178d501b095875059a277b07be4a
reebass <store1993dron@gmail.com>
2022-11-06 12:30:34 +0300

    first_task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galletyRefs = document.querySelector('.gallery');
+// let showImage;
+
+const createMarkUp = galleryItems.map(({preview, original, description}) => {
+            return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>`
+        }).join("");
+
+galletyRefs.innerHTML = createMarkUp;
+
+new SimpleLightbox(".gallery a", {
+captionsData: "alt",
+captionDelay: 250,
+});
