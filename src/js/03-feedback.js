6487c23c3d7284891cdfb00c485d6c9dd58fc708
reebass <store1993dron@gmail.com>
2022-11-08 19:49:14 +0200

    done-after-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,42 @@
 import throttle from "lodash.throttle";
-// console.log(throttle)
+
 
 const form = document.querySelector('.feedback-form')
-
-
-
 const email = form[0];
 const massage = form[1];
 
-
-const formValue = {}
+const formValue = {};
 
 addForm()
 
+
 form.addEventListener('input', throttle(onInput, 500));
-form.addEventListener('submit', onSubmit);
 
 function onInput () {
-    formValue.email = email.value;
-    formValue.massage = massage.value;
+        formValue.email = email.value,
+        formValue.massage = massage.value,
 
-    // console.log(formValue)
     
     localStorage.setItem('feedback-form-state', JSON.stringify(formValue))
 }
 
-function onSubmit (evt) {
-    localStorage.clear()
-    evt.currentTarget.reset();
 
+form.addEventListener('submit', onSubmit);
+
+function onSubmit (evt) {
+    evt.preventDefault();
+    if (email.value.length && massage.value.length) {
+                const userData = {
+                    email: email.value,
+                    massage: massage.value,
+                } 
+            console.log(userData)
+            }else {
+                return alert('Все поля должны быть заполнены')
+            }
+
+    localStorage.clear();
+    evt.target.reset();
 }
 
 function addForm() {
@@ -38,6 +46,8 @@ function addForm() {
     massage.value = saveValue.massage || "";
 }
 
-// console.log(saveValue)
+
 }
+
+
 
